f603b85ff62cdba07c97db3f34a3ca2116bf25f3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 23:17:17 +0200

    vanilla for link ._.

--- js/launch.js
+++ js/launch.js
@@ -26,3 +26,26 @@ function launch() {
     launcher.on('debug', (e) => console.log(e));
     launcher.on('data', (e) => console.log(e));
 }
+
+function launchlatest() {
+    const nameuser = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/email.txt", 'utf-8');
+    const passuser = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/password.txt", 'utf-8');
+    let opts = {
+        clientPackage: null,
+        authorization: Authenticator.getAuth(nameuser, passuser),
+        root: jsonstorage.getDefaultDataPath() + "/minecraft",
+        version: {
+            number: "1.16.5",
+            type: "release"
+        },
+        memory: {
+            max: "8G",
+            min: "4G"
+        }
+    }
+
+    launcher.launch(opts);
+
+    launcher.on('debug', (e) => console.log(e));
+    launcher.on('data', (e) => console.log(e));
+}
