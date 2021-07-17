63dff525dc2d47793b6918305047451b83c07778
cook1e <vincent.schweiger@xolley.de>
2021-07-17 13:58:33 +0200

    launch stuff uses token now

--- js/launch.js
+++ js/launch.js
@@ -4,9 +4,8 @@ const filesystem = require('fs');
 const launcher = new Client();
 
 function launch() {
-	rawAuthObj = filesystem.readFileSync(jsonstorage.getDefaultDataPath()+"/.mcauth", 'utf-8');
-	console.log(rawAuthObj)
-	authObj = JSON.parse(rawAuthObj);
+    rawAuthObj = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/.mcauth", 'utf-8');
+    authObj = JSON.parse(rawAuthObj);
     let opts = {
         clientPackage: null,
         authorization: authObj,
@@ -16,7 +15,7 @@ function launch() {
             type: "release",
             custom: "Cosmo"
         },
-        javaPath: jsonstorage.getDefaultDataPath()+"/jre1.8.0_291/bin/java",
+        javaPath: jsonstorage.getDefaultDataPath() + "/jre1.8.0_291/bin/java",
         memory: {
             max: "6G",
             min: "4G"
@@ -30,17 +29,18 @@ function launch() {
 }
 
 function launch165() {
-    const nameuser = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/email.txt", 'utf-8');
-    const passuser = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/password.txt", 'utf-8');
+    rawAuthObj = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/.mcauth", 'utf-8');
+    authObj = JSON.parse(rawAuthObj);
+    console.log(authObj)
     let opts = {
         clientPackage: null,
-        authorization: Authenticator.getAuth(nameuser, passuser),
+        authorization: authObj,
         root: jsonstorage.getDefaultDataPath() + "/minecraft",
         version: {
             number: "1.16.5",
             type: "release"
         },
-        javaPath: jsonstorage.getDefaultDataPath()+"/jre1.8.0_291/bin/java",
+        javaPath: jsonstorage.getDefaultDataPath() + "/jre1.8.0_291/bin/java",
         memory: {
             max: "6G",
             min: "4G"
@@ -55,17 +55,17 @@ function launch165() {
 
 
 function launch17() {
-    const nameuser = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/email.txt", 'utf-8');
-    const passuser = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/password.txt", 'utf-8');
+    rawAuthObj = filesystem.readFileSync(jsonstorage.getDefaultDataPath() + "/.mcauth", 'utf-8');
+    authObj = JSON.parse(rawAuthObj);
     let opts = {
         clientPackage: null,
-        authorization: Authenticator.getAuth(nameuser, passuser),
+        authorization: authObj,
         root: jsonstorage.getDefaultDataPath() + "/minecraft",
         version: {
             number: "1.17",
             type: "release"
         },
-        javaPath: jsonstorage.getDefaultDataPath()+"/jdk-16.0.1/bin/java",
+        javaPath: jsonstorage.getDefaultDataPath() + "/jdk-16.0.1/bin/java",
         memory: {
             max: "6G",
             min: "4G"
