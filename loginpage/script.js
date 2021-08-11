b8ada87ecd3755d03b9f8c2e56e6370ddd84aa94
BuildTools <unconfigured@null.spigotmc.org>
2021-08-11 21:55:57 +0200

    pw saving

--- loginpage/script.js
+++ loginpage/script.js
@@ -3,22 +3,48 @@ const fs = require("fs");
 const { Authenticator } = require('minecraft-launcher-core');
 const defaultDataPath = storage.getDefaultDataPath();
 
+let state;
+
+if (fs.existsSync(defaultDataPath + "/.pw-saving")) {
+   const dat = fs.readFileSync(defaultDataPath + "/.pw-saving", "utf-8");
+   state = dat;
+} else {
+   state = false;
+}
+
+updatePassState();
+
 async function setCredsMojang() {
    username = document.getElementById("email").value;
    password = document.getElementById("password").value;
    try {
+      if (state) {
+         const json = '{"username":"' + username + '","password":"' + password + '"}';
+         fs.writeFileSync(defaultDataPath + "/.mc-creds", json);
+      }
       auth = await Authenticator.getAuth(username, password);
       fs.writeFileSync(defaultDataPath + "/.mcauth", JSON.stringify(auth));
       document.location.replace("../index.html");
    }
    catch (e) {
       console.error(e);
-      alert("Something went wrong while logging in, try checking if your password/email are correct!");
+      alert("Something went wrong whilst logging in, try checking if your password/email are correct!");
    }
 }
 
+async function togglePwSafe() {
+   state = !state;
+   updatePassState();
+}
 
 
+function updatePassState() {
+   fs.writeFileSync(defaultDataPath + "/.pw-saving", state);
+   document.getElementById("safepass-state").innerHTML = "Password saving: " + state;
+}
+
+
+document.getElementById("safe-password").onclick = togglePwSafe;
 document.getElementById("mojang-login").onclick = loginfunc;
 function loginfunc() {
    setCredsMojang();
@@ -28,3 +54,18 @@ $('form').on('submit', function (event) {
    event.preventDefault();
 });
 
+async function foo() {
+   if (state == true) {
+      try {
+         const dat = fs.readFileSync(defaultDataPath + "/.mc-creds");
+         auth = await Authenticator.getAuth(dat["username"], dat["password"]);
+         fs.writeFileSync(defaultDataPath + "/.mcauth", JSON.stringify(auth));
+         document.location.replace("../index.html");
+      }
+      catch (e) {
+         console.error(e);
+         alert("Something went wrong whilst automatically logging in, try checking if your password/email are correct!");
+      }
+   }
+};
+foo();
