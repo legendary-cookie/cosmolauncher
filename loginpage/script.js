3e104b77bc31a433cb081049230c98cf1905a7ba
cook1e <vincent.schweiger@xolley.de>
2021-08-23 18:43:17 +0200

    stuff

--- loginpage/script.js
+++ loginpage/script.js
@@ -2,6 +2,8 @@
 /* eslint-disable no-unused-vars */
 const storage = require("electron-json-storage");
 const fs = require("fs");
+var http = require("follow-redirects").http;
+const { post } = require("jquery");
 const { Authenticator } = require("minecraft-launcher-core");
 const defaultDataPath = storage.getDefaultDataPath();
 
@@ -27,7 +29,38 @@ function loginfunc() {
 
 document.getElementById("microsoft-login").onclick = microsoft_login;
 async function microsoft_login() {
-   
+	var options = {
+		"method": "POST",
+		"hostname": "mc.vincentschweiger.de",
+		"port": 25566,
+		"path": "/msauth",
+		"headers": {
+			"Content-Type": "application/json"
+		},
+		"maxRedirects": 20
+	};
+	var req = http.request(options, function (res) {
+		var chunks = [];
+		res.on("data", function (chunk) {
+			chunks.push(chunk);
+		});
+		res.on("end", function (chunk) {
+			var body = Buffer.concat(chunks);
+			const token = JSON.parse(body.toString())["access_token"];
+
+		});
+		res.on("error", function (error) {
+			alert(error);
+			console.error(error);
+		});
+	});
+	var postData = JSON.stringify({
+		"username": document.getElementById("email").value,
+		"password": document.getElementById("password").value
+	});
+	req.write(postData);
+	req.end();
+	
 }
 
 $("form").on("submit", function (event) {
